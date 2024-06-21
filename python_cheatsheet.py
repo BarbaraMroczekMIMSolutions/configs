5d4a3ae15893e8fbc3e9f2b7782ce5eecf901235
Barbara Mroczek <barbara.mroczek@mim.ai>
2024-06-21 18:41:18 +0200

    misc

--- python_cheatsheet.py
+++ python_cheatsheet.py
@@ -1,43 +1,39 @@
 # pathlib
 
 from pathlib import Path
-
 wd = Path('.')
 
 # iteracje
 
 for file in wd.iterdir:
     pass
-
 for file in wd.glob("*"):
     pass
-
 for file in wd.rglob("*"):
     pass
 
 # sprawdzenia
 
 wd.is_dir() == True
-
 wd.exists() == True
-
 wd.mkdir(parents=True, exist_ok=True)
 
 # części ścieżki
 
 p = Path("/usr/bin/python3")
-
 p.parts == ('/', 'usr', 'bin', 'python3')
-
 p.parent == Path("/usr/bin")
-
 p.parents ==  # TODO
 p.parents[1] == Path("/usr")
-
 p.root == "/"
 
-
 import shutil
-
 shutil.copyfile(src, dst)
+
+
+# notebooks
+
+import os
+while "notebooks" in os.getcwd():
+    os.chdir("..")
 
